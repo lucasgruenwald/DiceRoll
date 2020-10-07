241eefb940b10a37dd5f53e3bfcd82bc83815d85
lukegruenwald <lgruenwald@sbcglobal.net>
2020-10-06 21:33:01 -0700

    Updates

---
+++ precache-manifest.2bdb767e60e608c22b8ba0062c233fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca2e5a876e80ddaeec1f46ddb55ec5b",
+    "url": "/://lucasgruenwald.github.io/RollDice/index.html"
+  },
+  {
+    "revision": "d2731033d83318fa0099",
+    "url": "/://lucasgruenwald.github.io/RollDice/static/css/main.747d84ec.chunk.css"
+  },
+  {
+    "revision": "1387e99572c09b10edfc",
+    "url": "/://lucasgruenwald.github.io/RollDice/static/js/2.67e6cd73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/://lucasgruenwald.github.io/RollDice/static/js/2.67e6cd73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2731033d83318fa0099",
+    "url": "/://lucasgruenwald.github.io/RollDice/static/js/main.ef75e421.chunk.js"
+  },
+  {
+    "revision": "801ec3e7a60d36f11306",
+    "url": "/://lucasgruenwald.github.io/RollDice/static/js/runtime-main.334d8fef.js"
+  }
+]);
